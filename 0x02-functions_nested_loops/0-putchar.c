06816760bed54a30f0519408ab35c5b8d54b77d1
Reem Abdelhaleem <reemhalim16062001@gmail.com>
2023-02-22 01:23:10 -0800

    10

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+README.md char *sh = "_putchar";
+
+README.md while (*sh)
+README.md {
+README.md README.md _putchar(*sh);
+README.md README.md sh++;
+README.md }
+README.md _putchar(n);
+
+README.md return (0);
+}
